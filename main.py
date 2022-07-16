a858e3e6375fac159c75aa71962ab7a46376ebab
xicotrainer <xicotraider@gmail.com>
2022-07-15 22:00:14 -0500

    Post methods load information throught: a) json, applyng an automatic serialization; or b) data, with customized serialization

--- main.py
+++ main.py
@@ -3,13 +3,23 @@ import json
 
 if __name__ == '__main__':
     url = 'https://httpbin.org/post'
-    arg = {
+    payload = {
         'name': 'Ramanujan',
         'course': 'api_rest',
         'level': 'basic'
     }
 
-    response = requests.post(url, params=arg)
+    """
+    response = requests.post(url, json=payload)
+    
+    if response.status_code == 200:
+        response_json = json.loads(response.text)
+        response_json = json.dumps(response_json, indent=2)
+
+        print(response.url, response_json, end='\n')
+    """
+
+    response = requests.post(url, data=json.dumps(payload))
     
     if response.status_code == 200:
         response_json = json.loads(response.text)

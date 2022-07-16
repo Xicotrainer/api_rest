b9744b1b37fb4428179f9b85ba81621fbeed21eb
xicotrainer <xicotraider@gmail.com>
2022-07-15 21:41:37 -0500

    We create a better printing shell view and begins with the post method testing

--- main.py
+++ main.py
@@ -2,20 +2,17 @@ import requests
 import json
 
 if __name__ == '__main__':
-    url = 'https://httpbin.org/get'
+    url = 'https://httpbin.org/post'
     arg = {
         'name': 'Ramanujan',
         'course': 'api_rest',
         'level': 'basic'
     }
 
-    response = requests.get(url, params=arg)
+    response = requests.post(url, params=arg)
     
     if response.status_code == 200:
-        """
-        response_json = response.json()
-        print(response.url, response_json['origin'], end='\n')
-
-        """
         response_json = json.loads(response.text)
-        print(response_json['origin'])
+        response_json = json.dumps(response_json, indent=2)
+
+        print(response.url, response_json, end='\n')

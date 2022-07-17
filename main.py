f5b4af18c1ab0524560b3220dc306d43b32a052d
xicotrainer <xicotraider@gmail.com>
2022-07-17 14:00:45 -0500

    We add the header functionality inside reuests' parametters

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import requests
 import json
 
+def jsonify(response):
+    response_json = json.loads(response.text)
+    response_json = json.dumps(response_json, indent=4)  
+    
+    return response_json
+
+
 if __name__ == '__main__':
     url = 'https://httpbin.org/post'
     payload = {
@@ -8,21 +15,14 @@ if __name__ == '__main__':
         'course': 'api_rest',
         'level': 'basic'
     }
+    headers = {
+        'Conten-Type': 'application/json',
+        'access-token': '12345'
+    }
 
-    """
-    response = requests.post(url, json=payload)
+    response = requests.post(url, data=json.dumps(payload), headers=headers)
     
     if response.status_code == 200:
-        response_json = json.loads(response.text)
-        response_json = json.dumps(response_json, indent=2)
-
-        print(response.url, response_json, end='\n')
-    """
-
-    response = requests.post(url, data=json.dumps(payload))
-    
-    if response.status_code == 200:
-        response_json = json.loads(response.text)
-        response_json = json.dumps(response_json, indent=2)
-
-        print(response.url, response_json, end='\n')
+        print(response.url)
+        print(jsonify(response))
+        print(response.headers['Server'])

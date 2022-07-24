570e74b3fbf13fa05817fd28dd7112ea55d7dd22
EcBalam <ec.balam.4t0@gmail.com>
2022-07-24 09:12:10 -0500

    This time, We accessed an URL in order to save its content

--- main.py
+++ main.py
@@ -1,34 +1,11 @@
 import requests
-import json
-
-def jsonify(response):
-    response_json = json.loads(response.text)
-    response_json = json.dumps(response_json, indent=4)  
-    
-    return response_json
-
 
 if __name__ == '__main__':
-    url = 'https://httpbin.org/delete'
-    payload = {
-        'name': 'Ramanujan',
-        'course': 'api_rest',
-        'level': 'basic'
-    }
-    headers = {
-        'Conten-Type': 'application/json',
-        'access-token': '12345'
-    }
-
-    response = requests.delete(url, data=json.dumps(payload), headers=headers)
-
-    # This is the CRUD methods for http:
-        # GET. To abtain a resource item
-        # POST. To create a resource item
-        # PUT. To update a resource item
-        # DELETE. To remove a resource item
+    url = 'https://i.imgur.com/cK64mEv.jpeg'
     
-    if response.status_code == 200:
-        print(response.url)
-        # print(jsonify(response))
-        print(response.headers['Server'])
+    response = requests.get(url, stream=True) #Make the request without downloading the content
+    with open('doggy_image.jpg', 'wb') as file:
+        for chunk in response.iter_content(): # Download the contentent by segments
+            file.write(chunk)
+
+    response.close()

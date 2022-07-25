aad3f841296e4dfdaaac3a8fb44a110e98f3bad5
EcBalam <ec.balam.4t0@gmail.com>
2022-07-25 13:43:35 -0500

    Using the pokemon's Api to make some requierement; we list the names for the endpoint ' https://pokeapi.co/api/v2/pokemon-form/'

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+#!/usr/bin/env python
+
 import requests
 
-if __name__ == '__main__':
-    url = 'https://pokeapi.co/api/v2/pokemon-form/'
-    
-    response = requests.get(url)
+def get_pokemons(url='https://pokeapi.co/api/v2/pokemon-form/', offset=0):
+    args = {'offset': offset} if offset else {}
 
-    if response.status_code == 200:
+    response = requests.get(url, params=args)
+
+    if response.status_code == 200: # Completed request code
+
         payload = response.json()
         results = payload.get('results', [])
 
@@ -13,3 +16,11 @@ if __name__ == '__main__':
             for pokemon in results:
                 name = pokemon['name']
                 print(name)
+        next = input("Does the list continue? [Y/N]").lower()
+        if next == 'y':
+            get_pokemons(offset=offset+20)
+
+if __name__ == '__main__':
+    url = 'https://pokeapi.co/api/v2/pokemon-form/'
+
+    get_pokemons()
